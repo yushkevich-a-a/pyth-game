82131001a30a1dc309d3fe7ba5ad317be1f24bbf
Anton <yushkevich.a.a@yandex.ru>
2024-07-09 09:15:56 +0300

    создание минимальной архитектуры приложения

---
+++ map.py
@@ -0,0 +1,18 @@
+# 🟩 ⬛ 🌊 🌳 ❤️ 🏥 ❤️ 🔴 🔥 ☁️  🌩️ 🌥️ 🚁 
+     
+
+class Map(object):
+  def __init__(self, w: int, h: int):
+      self.cels = [[ 0 for i in range(w)] for i in range(h)]
+
+  # генерация лесов
+  # def generate_forest(self):
+  #    return 0
+  
+  # генерация пожаров
+  # def generate_fire(self):
+  #    return 0
+  
+  def print_map(self):
+     return 0
+  
